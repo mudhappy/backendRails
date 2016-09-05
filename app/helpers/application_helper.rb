023f455de88437b3684b80222069a12ba5a7ca6c
mudhappy <mudhappy@hotmail.com>
2016-09-05 12:43:34 -0500

    Parciales para no repetir codigo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+	def resource
+		#Si no está declarado o es nulo asignale este valor
+		@resource ||= User.new
+	end
+	def resource_name
+		#Recurso asignado
+		:user
+	end
+
+	def resource_class
+		#Cual es la clase del recurso
+		User
+	end
 end
